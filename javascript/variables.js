791d9b1d77a4418c1b52232fcafb59023ca31431
shubham <shubhampal2002.ldh@gmail.com>
2025-02-25 21:54:28 +0530

    Learn JS

---
+++ javascript/variables.js
@@ -0,0 +1,26 @@
+// console.log("welcone to js")
+// console.log("hello world")/
+  
+
+// string datatypes
+// var my_firstname=("shubham")
+// console.log(my_firstname)
+
+
+// number datatypes
+// var myNum=(5)
+// console.log(myNum)
+
+// boolean datatypes
+// var isRaining=(true)
+// var youAwsem=(false)
+// console.log(isRaining , youAwsem)
+
+
+// var myString = ("55");
+// var myNumber = parseInt(myString)
+// console.log(myNumber)
+ 
+var myString = ("55.6");
+var myNumber = parseFloat(myString)
+ console.log(myNumber)
