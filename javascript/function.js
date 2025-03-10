31d87baeaa5af034b8a49fd3093241ef978b7b75
shubham <shubhampal2002.ldh@gmail.com>
2025-03-10 22:15:31 +0530

    modified - function

--- javascript/function.js
+++ javascript/function.js
@@ -1,3 +1,6 @@
+
+// ? function
+
 // function sum() {
 //     var a = 5,
 //     b= 10;
@@ -9,14 +12,32 @@
 
 // function greet (name){
 //     console.log("Hello " + name + "", ", welcone to my kingdom");
-    
+
 // }
 // greet("shubham")
 // greet("paul")
 
-function sum (a,b){
-    console.log(a+b);
-    
-}
-sum(5,8)
-sum(10,55)
+// function sum (a,b){
+//     console.log(a+b);
+
+// }
+// sum(5,8)
+// sum(10,55)
+
+// function sum (a,b){
+//    return a+b;
+
+// }
+// var result = sum (10,5)
+// console.log(result);
+
+// Immediately invoked function expression with parameters
+
+var result = (function sum(a, b) {
+    console.log(a + b)
+    return a+b;
+})(5, 5)
+
+console.log("the result is ",result);
+
+
