40e32ec1ef160c69373e2565906a47cb0be9dd26
shubham <shubhampal2002.ldh@gmail.com>
2025-03-11 21:56:33 +0530

    modified --ECMAscript- ES6

--- javascript/ECMAscript/ES6.js
+++ javascript/ECMAscript/ES6.js
@@ -19,11 +19,104 @@
 
 
 // const datatype ....const is used for constant value which is not change it 
-const myName = "shubham";
-if (true)  {
-    const myName = "paul"
-    console.log (myName)    
+// const myName = "shubham";
+// if (true)  {
+//     const myName = "paul"
+//     console.log (myName)    
+// }
+// myName = "shubhu"
+// console.log(myName);
+
+
+// template string  ..... ${} -- this template string
+
+// let firstName = "shubham";
+// let lastName = "pal";
+// let fullName =  `${ firstName } ${lastName}`;
+// console.log (fullName);
+
+// string interpolation
+
+// let age = 23;
+// let message = ` I am ${age} years old `;
+// console.log(message);
+
+// let multilineString =`My name is Shubham Pal.
+// I am 23 years old.
+// I live in Ludhiana (Punjab).`
+// console.log(multilineString);
+
+// let num = 5;
+// for (let i = 1; i <= 10; i++) {   
+//     console.log(`5 * ${i} = ${5 * i}`);  ////-- This is tempalte string interpolation 
+// }
+
+
+// Default Function parameter
+
+// function sum(a,b) {
+//     return a+b;    
+// }
+// console.log(sum(5,10)); 
+
+//--But if default funtion parameters
+
+// function sum(a = 5, b = 10) {
+//     return a + b;
+// }
+// console.log(sum(4));
+
+
+
+// Fat Arrow Funtion
+
+// const sum = function(a,b){
+// console.log(`The sum of two number is ${a+b}`);  ////--- this is only function without function name 
+
+// }
+// sum(5,5)
+
+// const sum = (a, b) => console.log(`The sum of two number is ${a + b}`);
+// sum(5, 5)  ///-- this is fat arrow function
+
+// --we can aslo write fat arrow function given in below
+// const sum = (a, b) => {
+//     console.log(`The sum of two number is ${a + b}`);
+// }
+// sum(5, 5)
+
+
+// practice question
+
+function calculator(a, b, operator) {
+    let result;
+    switch (operator) {
+
+        case '+':
+            result = a + b;
+            return result;
+
+        case '-':
+            result = a - b;
+            return result;
+        case '%':
+            result = a % b;
+            return result;
+        case '/':
+            result = a / b;
+            return result;
+
+
+        default:
+            return 'No operator found';
+
+    }
 }
-myName = "shubhu"
-console.log(myName);
+console.log(calculator(5, 3, '+'));
+console.log(calculator(5, 3, '-'));
+console.log(calculator(5, 3, '%'));
+console.log(calculator(5, 3, '/'));
+
+
+
 
