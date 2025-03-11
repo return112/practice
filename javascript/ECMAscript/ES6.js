c1fd2b4f894cf082edb17db9cd7f89ee2945bce1
shubham <shubhampal2002.ldh@gmail.com>
2025-03-11 16:59:45 +0530

    ECMAscript - ES6

---
+++ javascript/ECMAscript/ES6.js
@@ -0,0 +1,29 @@
+// var datatype ....var is used anywhere because it is global used datatype
+// var myName = "shubham";
+// if (true)  {
+//     myName = "paul"
+//     console.log (myName)    
+// }
+// myName = "shubhu"
+// console.log(myName);
+
+
+// let datatype....let is used only in blocked code ..thi is not used global 
+// let myName = "shubham";
+// if (true)  {
+//     let myName = "paul";   // this is   used inly in if blocked 
+//     console.log (myName) 
+// }
+// myName = "shubhu"
+// console.log(myName);
+
+
+// const datatype ....const is used for constant value which is not change it 
+const myName = "shubham";
+if (true)  {
+    const myName = "paul"
+    console.log (myName)    
+}
+myName = "shubhu"
+console.log(myName);
+
